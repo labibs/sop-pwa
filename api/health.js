e17ab0af448d0cc78ecc72eb8e172ebf6e0a994d
Labib suturi <visioner.lv@gmail.com>
2026-07-09 21:20:40 +0700

    perbaikan error vercel

--- api/health.js
+++ api/health.js
@@ -1,13 +1,13 @@
-import { sendWebResponse } from "./_node.js";
-import { json } from "./_shared.js";
-
-export default async function handler(_req, res) {
-  await sendWebResponse(
-    res,
-    json({
+export default function handler(_req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.setHeader("Cache-Control", "no-store");
+  res.end(
+    JSON.stringify({
       ok: true,
       adminPassword: Boolean(process.env.ADMIN_PASSWORD),
       blobToken: Boolean(process.env.BLOB_READ_WRITE_TOKEN),
+      node: process.version,
     }),
   );
 }
